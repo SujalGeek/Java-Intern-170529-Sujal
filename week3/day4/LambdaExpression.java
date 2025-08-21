765c436a00eb9c94dc3d1b71cd8f227c22c9e34a
SujalGeek <morwanisujal875@gmail.com>
2025-08-21 18:14:09 +0530

    week4 day3 progress

--- week3/day4/LambdaExpression.java
+++ week3/day4/LambdaExpression.java
@@ -1,5 +1,16 @@
 package week3.day4;
 
-public class LambdaExpression {
+import java.util.ArrayList;
 
+public class LambdaExpression {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    numbers.add(5);
+    numbers.add(9);
+    numbers.add(8);
+    numbers.add(1);
+    numbers.forEach((n) -> {
+      System.out.println(n);
+    });
+  }
 }
