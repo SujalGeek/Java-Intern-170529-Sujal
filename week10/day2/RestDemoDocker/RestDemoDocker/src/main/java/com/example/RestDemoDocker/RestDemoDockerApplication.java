e4e29eed23c4102712cf339d7b11003847d2d8bb
SujalGeek <morwanisujal875@gmail.com>
2025-10-07 18:06:53 +0530

    week10 day2 progress

---
+++ week10/day2/RestDemoDocker/RestDemoDocker/src/main/java/com/example/RestDemoDocker/RestDemoDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestDemoDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemoDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemoDockerApplication.class, args);
+	}
+
+}
