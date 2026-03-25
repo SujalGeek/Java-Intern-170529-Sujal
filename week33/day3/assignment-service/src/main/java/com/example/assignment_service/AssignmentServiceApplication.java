db91a75913868f6e36c2efc3f9f4c0d3c2cc1573
SujalGeek <morwanisujal875@gmail.com>
2026-03-25 17:56:41 +0530

    fixed the frontend bug and started the kubernetes but error while integrating

---
+++ week33/day3/assignment-service/src/main/java/com/example/assignment_service/AssignmentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.assignment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class AssignmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentServiceApplication.class, args);
+	}
+
+}
